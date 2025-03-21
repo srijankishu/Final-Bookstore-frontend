0e0d714df19f059f65a012e761415081c69a2530
srijankishu <140534769+srijankishu@users.noreply.github.com>
2025-03-21 22:24:25 +0530

    Update Course.jsx

--- src/Component/Course.jsx
+++ src/Component/Course.jsx
@@ -12,7 +12,7 @@ const Course = () => {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("https://final-bookstore-backend.vercel.app/book");
         setBook(res.data);
       } catch (error) {
         console.log(error);
