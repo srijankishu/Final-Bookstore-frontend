933045b14a5a8ac5945ab602c6dee645eccc5eda
srijankishu <140534769+srijankishu@users.noreply.github.com>
2025-04-09 23:09:28 +0530

    Update Signup.jsx

--- src/Component/Signup.jsx
+++ src/Component/Signup.jsx
@@ -50,7 +50,7 @@ export const Signup = () => {
               <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
             </svg>
             <input type="text" 
-             className="grow p-2 border border-black text-black rounded-md outline-none" 
+             className="grow p-2 border border-black dark:text-white text-black rounded-md outline-none" 
              placeholder="Username"
              {...register('fullname', { required: true })} />
           </label>
@@ -61,7 +61,7 @@ export const Signup = () => {
               <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
             </svg>
             <input type="text" 
-             className="grow p-2 border border-black text-black rounded-md outline-none" 
+             className="grow p-2 border border-black dark:text-white text-black rounded-md outline-none" 
              placeholder="Email" 
              {...register('email', { required: true })}/>
           </label>
@@ -71,7 +71,7 @@ export const Signup = () => {
               <path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" />
             </svg>
             <input type="password" 
-             className="grow p-2 border text-black border-black rounded-md outline-none" 
+             className="grow p-2 border dark:text-white text-black border-black rounded-md outline-none" 
              placeholder="Password" 
              {...register('password', { required: true })}/>
           </label>
